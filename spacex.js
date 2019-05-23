0dedbf81deb7fd8d02496420e8441e2cc9bd7f5a
Beyonce Knowles <beyonce@thinkful.com>
2019-05-23 17:05:12 -0500

    update ARIA

--- spacex.js
+++ spacex.js
@@ -13,7 +13,7 @@ function getMissionPatch() {
 function displayMissionPatch(responseJson) {
     for (let i=0; i < responseJson.length; i++ ){
       if (responseJson[i].links.mission_patch !== null) {
-        $('.mission').append(`<a href="mission.html"><div class="mission-container" id="${responseJson[i].flight_number - 1}"><h3 class="flight-number">Flight Number ${responseJson[i].flight_number}</h3><h4 class="mission-name">${responseJson[i].mission_name}</h4><img src="${responseJson[i].links.mission_patch}" class="mission-patch"></div></a>`);
+        $('.mission').append(`<a href="mission.html"><div class="mission-container" id="${responseJson[i].flight_number - 1}"><h3 class="flight-number">Flight Number ${responseJson[i].flight_number}</h3><h4 class="mission-name">${responseJson[i].mission_name}</h4><img src="${responseJson[i].links.mission_patch}" class="mission-patch" alt="${responseJson[i].mission_name} mission patch"></div></a>`);
         }
     }
 }
@@ -42,7 +42,7 @@ function displayMissionPatch(responseJson) {
       responseJson.rocket.first_stage.cores[0].land_success = "False";
       responseJson.details = "None";
     }
-    $('.individual-mission').prepend(`<img src="${responseJson.links.mission_patch_small}"><div class="mission-details">
+    $('.individual-mission').prepend(`<img src="${responseJson.links.mission_patch_small} alt="${missionName} mission patch"><div class="mission-details">
     <h3 class="name">${responseJson.mission_name}</h3>
     <ul><li><span class="bold-text">Flight Numer:</span> ${responseJson.flight_number}</li>
     <li><span class="bold-text">Launch Year:</span> ${responseJson.launch_year}</li>
@@ -52,7 +52,7 @@ function displayMissionPatch(responseJson) {
     <li><span class="bold-text">Launch Success:</span> ${responseJson.launch_success}</li>
     <li><span class="bold-text">Land Success:</span> ${responseJson.rocket.first_stage.cores[0].land_success}</li>
     <li><span class="bold-text">Details:</span> ${responseJson.details}</li>
-    </ul></div><hr>`);
+    </ul></div>`);
 
     if (imageCount > 0) {
         $('.mission-images').append(`
@@ -65,7 +65,7 @@ function displayMissionPatch(responseJson) {
         for (let i=0; i < imageCount; i++) {
           $('.mission-images').append(`
           <div class="mySlides fade">
-          <img class="images" src="${responseJson.links.flickr_images[i]}">
+          <img class="images" src="${responseJson.links.flickr_images[i]}" alt="mission images">
           </div>`);
         }
         showSlides(slideIndex);
@@ -74,12 +74,6 @@ function displayMissionPatch(responseJson) {
     watchForm();
   }
   
-// window.onpopstate = function (event) {
-//   if (event.state) { state = event.state; }
-//   render(state);
-// };
-
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
